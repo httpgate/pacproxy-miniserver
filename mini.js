d43282d29fb3e344aac02600cdc89e7cb3941f6d
httpgate <73725631+httpgate@users.noreply.github.com>
2024-05-02 05:51:45 -0400

    update  to pacproxy-server v1.4.0

--- mini.js
+++ mini.js
@@ -3,7 +3,7 @@ var configs =
 {
 	//pac链接， 设置在浏览器里的格式是：https://your.site.domain/pac链接, 请尽量改成长一点的随机字符：'/xxxxxxxx'
 	paclink : '/0000000000000000',
-	//你申请到的网站域名, 同时只能运行一个域名
+	//你申请到的网站域名
 	domain : 'your.site.domain',
 	//伪装成海外站点url, 格式为: https://blog.ddns.com/homepage.htm, 请不要用国内网站伪装
 	website :  'https://en.minghui.com',
@@ -11,10 +11,8 @@ var configs =
 	iphours : 2	,
 	//绑定用户名密码的pac链接, 默认是空[], 数据格式是:['paclink', 'username', 'password'], 浏览器会提示输入用户密码
 	pacpass : [],  //数据格式：['/1111111111111111', 'proxyuser', 'proxypass'],	
-    //管理员email,数字证书到期会受到email通知	
+    //管理员email, 必须填，数字证书到期会受到email通知	
 	email : '',
-    //如果数字证书过期了，需要续数字证书时，需要把forcert改成true, 重启服务器，访问网站等数字证书更新后再改回来重启网站
-	forcert : false
 
 };
 
